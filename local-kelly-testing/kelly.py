f5a15d449bfaa54d187e2a273b94892122479374
Dan Carpenter <dancarpenter21@gmail.com>
2025-07-29 18:15:03 -0400

    Migrated to data frames and pandas

--- local-kelly-testing/kelly.py
+++ local-kelly-testing/kelly.py
@@ -1,51 +1,18 @@
-import sys
 import random
-
-class Result:
-
-    def __init__(self, initial_b, final_b, result, f, p, b, wager, payout, day):
-        '''
-        Should make this a pandas dataframe for real data analysis
-        '''
-        self.initial_b = initial_b
-        self.final_b = final_b
-        self.result = result
-        self.f = f
-        self.p = p
-        self.b = b
-        self.wager = wager
-        self.day = day
-        self.payout = payout
-
-    def __str__(self):
-        return f'Result[Day: {self.day} Bank: {self.initial_b:.2f}->{self.final_b:.2f}   f: {self.f:.2f}={self.p:.2f}-(1-{self.p:.2f})/{self.b:.2f}   {self.result=} {self.wager=:.2f} {self.payout=:.2f}]'
-
+import pandas as pd
 
 class KellyBasic:
 
     def __init__(self, bankroll=20):
         self._bankroll = bankroll
-        self._results = []
-
-    def print_results(self, start=0, end=sys.maxsize):
-        if end > len(self._results):
-            end = len(self._results)
-
-        for i in range(start, end):
-            print(self._results[i])
+        self._columns = ['Day', 'Starting Bank', 'Ending Bank', 'Result', 'Wager', 'Payout', 'f', 'b', 'p']
+        self._results = pd.DataFrame(columns=self._columns)
 
     def get_results(self):
         return self._results
 
     def get_bankroll(self):
         return self._bankroll
-
-    def get_bankline(self):
-        bankline = []
-        for r in self._results:
-            bankline.append(r.final_b)
-
-        return bankline
 
     def simulate_days(self, days, p_expected=0.7, b_mean=0.7):
         for d in range(days):
@@ -72,8 +39,8 @@ class KellyBasic:
             self._bankroll += r * payout
 
             # save off result
-            result = Result(result=r, f=f, p=p, b=b, wager=wager, day=len(self._results)+1, initial_b=initial_b, final_b=self._bankroll, payout=payout)
-            self._results.append(result)
+            #columns = ['Day', 'Starting Bank', 'Ending Bank', 'Result', 'Wager', 'Payout', 'f', 'b', 'p']
+            self._results.loc[len(self._results)] = [d+1, initial_b, self._bankroll, r, wager, payout, f, b, p]
 
     def _update_b(self, b):
         spread = random.random() * 0.2 - 0.1
--- local-kelly-testing/main.py
+++ local-kelly-testing/main.py
@@ -1,57 +1,62 @@
 import numpy as np
-import matplotlib
 import matplotlib.pyplot as plt
 
 from argparse import ArgumentParser
 from kelly import KellyBasic, KellyAdaptive
 
-print("Kelly Computations")
+def scatter(statics, outfile):
+    xd = []
+    yd = []
+    for k in statics:
+        # ['Day', 'Starting Bank', 'Ending Bank', 'Result', 'Wager', 'Payout', 'f', 'b', 'p']
+        res_frame = k.get_results()
+        observed_win_prob = res_frame['Result'].sum() / len(res_frame)
+        xd.append(observed_win_prob)
+        average_bankroll = res_frame['Ending Bank'].mean()
+        yd.append(average_bankroll)
 
-parser = ArgumentParser()
+    x = np.array(xd)
+    y = np.array(yd)
 
-parser.add_argument('-d', '--days', type=int, default=30, help='Number of days to experiment with')
-parser.add_argument('-n', '--monte', type=int, default=1, help='Number of monte carlo runs')
-parser.add_argument('-b', '--bankroll', type=int, default=20, help='Starting bankroll')
-parser.add_argument('-m', '--max_bankroll', type=int, default=2000, help='Maximum bankroll to consider realistic')
-
-args = parser.parse_args()
-
-# generate a bunch of data
-statics = []
-adaptives = []
-
-for n in range(args.monte):
-    ks = KellyBasic(args.bankroll)
-    ks.simulate_days(args.days)
-    statics.append(ks)
-
-    ka = KellyAdaptive(args.bankroll)
-    ka.simulate_days(args.days)
-    adaptives.append(ka)
+    plt.scatter(x,y)
+    plt.xlabel('Observed Win Probability')
+    plt.ylabel('Average Bankroll')
+    plt.savefig(outfile)
 
 
-    #ks.print_results()
-    #ka.print_results()
 
-# plot ending bankrolls
-bankrolls_s = []
-for k in statics:
-    if k.get_bankroll() < args.max_bankroll:
-        bankrolls_s.append(k.get_bankroll())
 
-np_static = np.array(bankrolls_s)
-plt.hist(np_static, bins=300, edgecolor='black')
-plt.title(f'Static {len(bankrolls_s)} Eligible Runs')
-plt.savefig('out/static_hist.png')
-plt.clf()
+if __name__ == '__main__':
 
-bankrolls_a = []
-for k in adaptives:
-    if k.get_bankroll() < args.max_bankroll:
-        bankrolls_a.append(k.get_bankroll())
+    print("Kelly Computations")
 
-np_static = np.array(bankrolls_a)
-plt.hist(np_static, bins=300, edgecolor='black')
-plt.title(f'Adaptive {len(bankrolls_a)} Eligible Runs')
-plt.savefig('out/adaptive_hist.png')
-plt.clf()
+    parser = ArgumentParser()
+
+    parser.add_argument('-d', '--days', type=int, default=30, help='Number of days to experiment with')
+    parser.add_argument('-n', '--monte', type=int, default=1, help='Number of monte carlo runs')
+    parser.add_argument('-b', '--bankroll', type=int, default=20, help='Starting bankroll')
+    parser.add_argument('-m', '--max_bankroll', type=int, default=2000, help='Maximum bankroll to consider realistic')
+
+    args = parser.parse_args()
+
+    # generate a bunch of data
+    statics = []
+    adaptives = []
+
+    for n in range(args.monte):
+        ks = KellyBasic(args.bankroll)
+        ks.simulate_days(args.days)
+        statics.append(ks)
+        #print(ks.get_results())
+
+        ka = KellyAdaptive(args.bankroll)
+        ka.simulate_days(args.days)
+        adaptives.append(ka)
+        #print(ka.get_results())
+
+    # scatterplots of win p vs ending bankroll
+    scatter(statics, 'out/statics.png')
+    scatter(adaptives, 'out/adaptives.png')
+
+
+
